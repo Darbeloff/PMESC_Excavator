48c962a27e0df12b35a368fbe2a80a69953f8175
Weitung Chen <weitung@mit.edu>
2018-10-09 14:47:53 -0400

    modify bucket tip mapping not yet implement corner detection

--- src/opt_flow/src/BucketTipMappingCalibration.py
+++ src/opt_flow/src/BucketTipMappingCalibration.py
@@ -14,51 +14,61 @@ from common import anorm2, draw_str
 from time import clock
 from time import sleep 
 
-refPt = []
-BucketPos = 0.0
-index = 0
-Img_cv = []
-flag = False
+CALIBRATE_MODE = 0 	#0: Manual Calibration; 1: Use Corner Detection Method
 
 class BucketTipMapping:
 	def __init__(self):
 		rospy.init_node('BucketTipMappingNode', anonymous=True)
-		self.sub_Img = rospy.Subscriber('UBC_Image', Image, self.cb_Img)				#Subscribe the image Raw
-	
-	def click_and_crop(event, x, y, flags, param):
-		global refPt, image, flag
-	 
-		if event == cv2.EVENT_LBUTTONDOWN:
-			refPt = [(x, y)]
+		self.sub_Img = rospy.Subscriber('UBC_Image', Image, self.cb_Img)				#Subscribe the image Raw from the bucket video ros bag
+		self.sub_Cal   = rospy.Subscriber("cal_Data", JointState, self.cb_CalValue)		#Subscribe the Joint State
+		
+		# Initiate Necessary Parameters
+		self.rate = 100 				#[Hz] the spin speed of the ros
 
-		elif event == cv2.EVENT_LBUTTONUP:
-			refPt.append((x, y))
-			cv2.line(Img_cv, refPt[0], refPt[1], (0, 255, 0), 2)
-			cv2.imshow("image", Img_cv)
-			flag = True
+		self.BucketPosition = 0.0
+		self.RawImage = []
+
+		##For Manual Calibration 
+		self.flag = False
+		self.refPt = []
+		self.index = 0
+
+	def cb_CalValue(msg):
+		self.BucketPosition = msg.position[2]
 
 	def cb_Img(msg):
-		global Img_cv, index, flag
 		bridge = CvBridge()
-	        image_decoded = bridge.imgmsg_to_cv2(msg,desired_encoding="bgr8")	#decode ROS messages to opencv format
-	        Img_cv = cv2.transpose(image_decoded)
-		"""
-		detect the position of the bucket tips
-		"""
-		cv2.imshow("image", Img_cv)
-	 	cv2.setMouseCallback("image", click_and_crop)
-		cv2.waitKey(0)
-		
-		if flag:
-			index = index + 1
-			print(str(msg.header.seq)+" "+str(refPt[0][0]) + " " + str(refPt[0][1]) + " " +str(refPt[1][0]) + " "+str(refPt[1][1]) + "  ")
-			flag = False
+	    image_decoded = bridge.imgmsg_to_cv2(msg,desired_encoding="bgr8")	#decode ROS messages to opencv format
+	    gray = cv2.cvtColor(cv2.transpose(image_decoded),cv2.COLOR_BGR2GRAY)
+		self.RawImage = np.float32(gray)
+	
+	def click_and_crop(event, x, y, flags, param):
+		if event == cv2.EVENT_LBUTTONDOWN:
+			self.refPt = [(x, y)]
+
+		elif event == cv2.EVENT_LBUTTONUP:
+			self.refPt.append((x, y))
+			cv2.line(self.RawImage, refPt[0], refPt[1], (0, 255, 0), 2)
+			cv2.imshow("image", self.RawImage)
+			self.flag = True
 
 	def update(self):
         r = rospy.Rate(self.rate)
         
-        
         while not rospy.is_shutdown():
+
+        	if CALIBRATE_MODE == 0:
+        		"""
+				Use mouse call back to get the tips of the buckets
+				"""
+        		cv2.imshow("image", se;f.RawImage)
+			 	cv2.setMouseCallback("image", self.click_and_crop)
+				cv2.waitKey(0)
+				
+				if flag:
+					self.index += 1
+					print(str(msg.header.seq)+" "+str(self.refPt[0][0]) + " " + str(self.refPt[0][1]) + " " +str(self.refPt[1][0]) + " "+str(self.refPt[1][1]) + "  ")
+					self.flag = False
 
             self.pub_arduino.publish(arduino_controller_msg)
             self.pub_dynamixel.publish(dynamixel_controller_msg)
